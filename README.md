ae93aae6a46b47caf9b5091acc19cc6f4b7ca27d
Anton Khartishov <night-fog@bk.ru>
2017-01-14 02:00:53 +0300

    working release, version 0.1

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+Данный скрипт предоставляет данные о барах Москвы, основываясь на данных с портала data.mos.ru
+В данный момент скрипт выводит:
+* самый большой бар;
+* самый маленький бар;
+* самый близкий бар (текущие gps-координаты ввести с клавиатуры).
+
 
 # Как запустить
 
@@ -11,8 +16,12 @@
 ```#!bash
 
 $ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
-
+# Пример вывода скрипта
+# Biggest bar is SПБ
+# Smallest bar is Бар в Деловом центре Яуза
+# Enter your latitude: 31
+# Enter your longitude: 73
+# Nearest bar is The Box
 ```
 
 Запуск на Windows происходит аналогично.
@@ -20,3 +29,5 @@ $ python bars.py # possibly requires call of python3 executive instead of just p
 # Цели проекта
 
 Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
+
+
--- bars.py
+++ bars.py
@@ -1,21 +1,60 @@
+# -*- coding: utf-8 -*-
+import urllib2
 import json
+
+MOSCOW_BARS_DATA_INFO = "http://api.data.mos.ru/v1/datasets/1796"
+API_ROWS_MAX = 500
 
 
 def load_data(filepath):
-    pass
+    bars_info_url_resp = urllib2.urlopen(filepath)
+    bars_data_info = json.loads(bars_info_url_resp.read())
+    bars_count = bars_data_info["ItemsCount"]
+    bars_list = []
+    for i in range(bars_count//API_ROWS_MAX+1):
+        get_bars_list_url = filepath + "/rows?$top=" + str(API_ROWS_MAX) + "&$skip=" + str((i+1)*API_ROWS_MAX)
+        bars_data_req = urllib2.urlopen(get_bars_list_url)
+        bars_data_dict = json.loads(bars_data_req.read())
+        for j in range(len(bars_data_dict)):
+            bars_list.append(bars_data_dict[j])
+    return bars_list
 
 
 def get_biggest_bar(data):
-    pass
+    biggest_bar_name = None
+    biggest_bar_seats_count = 0
+    for i in range(len(data)):
+        bar_seats_count = data[i].get("Cells").get("SeatsCount")
+        if bar_seats_count > biggest_bar_seats_count or biggest_bar_name is None:
+            biggest_bar_seats_count = bar_seats_count
+            biggest_bar_name = data[i].get("Cells").get("Name")
+    return biggest_bar_name
 
 
 def get_smallest_bar(data):
-    pass
+    smallest_bar_name = ""
+    smallestbar_seats_count = None
+    for i in range(len(data)):
+        bar_seats_count = data[i].get("Cells").get("SeatsCount")
+        if bar_seats_count < smallestbar_seats_count or smallestbar_seats_count is None:
+            smallestbar_seats_count = bar_seats_count
+            smallest_bar_name = data[i].get("Cells").get("Name")
+    return smallest_bar_name
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    nearest_bar_name = None
+    nearest_bar_k = None
+    for i in range(len(data)):
+        bar_coord = data[i].get("Cells").get("geoData").get("coordinates")
+        bar_k = abs(bar_coord[0]-longitude) + abs(bar_coord[1]-latitude)
+        if nearest_bar_k is None or bar_k < nearest_bar_k:
+            nearest_bar_name = data[i].get("Cells").get("Name")
+            nearest_bar_k = abs(bar_coord[0]-longitude) + abs(bar_coord[1]-latitude)
+    return nearest_bar_name
 
-
-if __name__ == '__main__':
-    pass
+barsList = load_data(MOSCOW_BARS_DATA_INFO)
+print ("Biggest bar is " + get_biggest_bar(barsList))
+print ("Smallest bar is " + get_smallest_bar(barsList))
+current_location = [input("Enter your latitude: "), input("Enter your longitude: ")]
+print ("Nearest bar is " + get_closest_bar(barsList, current_location[0], current_location[1]))
