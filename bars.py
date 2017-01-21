20eb7f8a1b4648a3615bb792117c0a25f8c72246
Anton Khartishov <night-fog@bk.ru>
2017-01-21 21:13:32 +0300

    working release v0.1.4 add read from input filepath add exception for wrong encoding

--- bars.py
+++ bars.py
@@ -3,18 +3,21 @@ import math
 
 __author__ = "Anton Khartishov"
 __license__ = "GPL"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __maintainer__ = "Anton Khartishov"
 __email__ = "night-fog@bk.ru"
 
 
 def read_bars_data_from_file(file_path):
     try:
-        with open(file_path, encoding='utf-8') as json_data:
+        with open(file_path, encoding='windows-1251') as json_data:
             data = json.load(json_data)
     except FileNotFoundError:
-            print("Caption or Id keys was not found in dataset")
-            raise
+        print("File " + file_path + " not found")
+        exit(1)
+    except UnicodeDecodeError:
+        print("Wrong file encoding. Use windows-1251.")
+        exit(1)
     return data
 
 
@@ -36,8 +39,9 @@ def get_closest_bar(data, longitude, latitude):
                                  latitude) ** 2))
     return closest_bar.get("Name")
 
+moscow_bars_json = input("Enter json file path: ")
+bars = read_bars_data_from_file(moscow_bars_json)
 
-bars = read_bars_data_from_file('moscowBars.json2')
 print("Biggest bar is :" + get_biggest_bar(bars))
 print("Smallest bar is :" + get_smallest_bar(bars))
 current_location = [input("Enter your latitude: "),
