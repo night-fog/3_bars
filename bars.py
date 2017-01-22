846b9f83b17b9b05211787395579983c29847a78
Anton Khartishov <night-fog@bk.ru>
2017-01-23 00:02:33 +0300

    working release v0.1.6 rem:main - script additional data info mod:fun def read_bars_data_from_file - exceptions moved to main code mod:main - parser config moverd to func parser_config mod:main - add usage of global var __name__ for main code

--- bars.py
+++ bars.py
@@ -2,24 +2,24 @@ import json
 import math
 import argparse
 
-__author__ = "Anton Khartishov"
-__license__ = "GPL"
-__version__ = "0.1.5"
-__maintainer__ = "Anton Khartishov"
-__email__ = "night-fog@bk.ru"
+
+def parser_config():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='''\
+            Read data from json file, set in first argument.
+            Find the largest bar in data.
+            Find the smallest bar in data.
+            Find the nearest bar by latitude and longitude.''')
+    parser.add_argument('file_path', metavar='bars.json', type=str,
+                        help='path to the json file with bars data')
+    return parser
 
 
 def read_bars_data_from_file(file_path):
-    try:
-        with open(file_path, encoding='windows-1251') as json_bars_data:
-            bars_data = json.load(json_bars_data)
-            return bars_data
-    except FileNotFoundError:
-        print("File " + file_path + " not found")
-        exit(1)
-    except UnicodeDecodeError:
-        print("Wrong file encoding. Use windows-1251.")
-        exit(1)
+    with open(file_path, encoding='windows-1251') as json_bars_data:
+        bars_data = json.load(json_bars_data)
+        return bars_data
 
 
 def get_biggest_bar(data):
@@ -40,26 +40,26 @@ def get_closest_bar(data, longitude, latitude):
                                 latitude) ** 2))
     return closest_bar.get("Name")
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    description='''\
-    Read data from json file, set in first argument.
-    Find the largest bar in data.
-    Find the smallest bar in data.
-    Find the nearest bar by latitude and longitude.''')
-parser.add_argument('file_path', metavar='bars.json', type=str,
-                    help='path to the json file with bars data')
 
-args = parser.parse_args()
-bars = read_bars_data_from_file(args.file_path)
-print("Biggest bar is : " + get_biggest_bar(bars))
-print("Smallest bar is : " + get_smallest_bar(bars))
-try:
-    current_location = [float(input("Enter your latitude: ")),
-                        float(input("Enter your longitude: "))]
-    nearest_bar_name = get_closest_bar(bars, current_location[0],
-                                       current_location[1])
-    print("Nearest bar is : " + nearest_bar_name)
-except ValueError:
-    print("Coordinates type mismatch")
-    exit(1)
+if __name__ == '__main__':
+    args = parser_config().parse_args()
+    try:
+        bars = read_bars_data_from_file(args.file_path)
+    except FileNotFoundError:
+        print("File " + args.file_path + " not found")
+        exit(1)
+    except UnicodeDecodeError:
+        print("Wrong file encoding. Use windows-1251.")
+        exit(1)
+
+    print("Biggest bar is : " + get_biggest_bar(bars))
+    print("Smallest bar is : " + get_smallest_bar(bars))
+    try:
+        current_location = [float(input("Enter your latitude: ")),
+                            float(input("Enter your longitude: "))]
+        nearest_bar_name = get_closest_bar(bars, current_location[0],
+                                           current_location[1])
+        print("Nearest bar is : " + nearest_bar_name)
+    except ValueError:
+        print("Coordinates type mismatch")
+        exit(1)
